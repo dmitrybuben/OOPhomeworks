e77e841692a5f5b3e10a63447f18c1a5e8fded60
dmitrybuben <97685811+dmitrybuben@users.noreply.github.com>
2022-08-07 21:10:18 +0300

    First class and readme file added

---
+++ src/sem1hw1/People.java
@@ -0,0 +1,34 @@
+package sem1hw1;
+
+public class People {
+    String name;
+    String surname;
+    int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public People(String name, String surname, int age) {
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "People{" +
+                "name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
