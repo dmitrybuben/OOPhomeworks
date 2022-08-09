a5ad0077e3ca197366e6d49b0683fede2c1683d1
dmitrybuben <97685811+dmitrybuben@users.noreply.github.com>
2022-08-09 15:15:55 +0300

    Classes added

---
+++ src/sem1hw1/Relation.java
@@ -0,0 +1,12 @@
+package sem1hw1;
+
+public enum Relation {
+    PARENT,
+    CHILD,
+    BROTHER,
+    SISTER,
+    GRANDFATHER,
+    GRANDSON,
+    AUNT,
+    NEPHEW
+}
---
+++ src/sem1hw1/Tree.java
@@ -0,0 +1,27 @@
+package sem1hw1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Tree {
+    private List<Link>links = new ArrayList<>();
+
+    public void addLink(Person p1,Person p2, Relation link1, Relation link2) {
+        Link l1 = new Link(p1, p2, link1);
+        Link l2 = new Link(p2, p1, link2);
+
+        links.add(l1);
+        links.add(l2);
+    }
+
+    public List<Link> getLinks() {
+        return links;
+    }
+
+    @Override
+    public String toString() {
+        return "Tree{" +
+                "links=" + links +
+                '}';
+    }
+}
